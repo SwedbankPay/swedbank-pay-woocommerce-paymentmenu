25975efd04a617c618b32f33d9fac1acd6773ba1
Montazar <github.v0its@slmail.me>
2025-08-10 23:21:23 +0200

    Set order as processing on authorization

--- includes/class-swedbank-pay-api.php
+++ includes/class-swedbank-pay-api.php
@@ -405,22 +405,16 @@ class Swedbank_Pay_Api {
 			case self::TYPE_VERIFICATION:
 				break;
 			case self::TYPE_AUTHORIZATION:
-				$message = sprintf( 'Payment has been authorized. Transaction: %s', $transaction_id );
+				// translators: 1: transaction ID.
+				$message = sprintf( __( 'Payment has been authorized. Transaction: %s', 'swedbank-pay-woocommerce-checkout' ), $transaction_id );
 
-				// Don't change the order status if it was captured before
-				if ( $order->has_status( array( 'processing', 'completed', 'active' ) ) ) {
+				if ( empty( $order->get_date_paid() ) ) {
+					$order->payment_complete( $transaction_id );
 					$order->add_order_note( $message );
-				} else {
-					$this->update_order_status(
-						$order,
-						'on-hold',
-						$transaction_id,
-						sprintf( 'Payment has been authorized. Transaction: %s', $transaction_id )
-					);
+					$order->save();
 				}
 
 				break;
-			case self::TYPE_CAPTURE:
 			case self::TYPE_SALE:
 				$is_full_capture = false;
 
@@ -585,7 +579,7 @@ class Swedbank_Pay_Api {
 		switch ( $status ) {
 			case 'checkout-draft':
 			case 'pending':
-				// Set pending
+				// Set pending.
 				if ( ! $order->has_status( 'pending' ) ) {
 					$order->update_status( 'pending', $message );
 				} elseif ( $message ) {
@@ -594,9 +588,10 @@ class Swedbank_Pay_Api {
 
 				break;
 			case 'on-hold':
-				// Set on-hold
+				// FIXME: Why do we need to reduce stock if it is on-hold?
+				// Set on-hold.
 				if ( ! $order->has_status( 'on-hold' ) ) {
-					// Reduce stock
+					// Reduce stock.
 					wc_maybe_reduce_stock_levels( $order_id );
 					$order->update_status( 'on-hold', $message );
 				} elseif ( $message ) {
@@ -608,7 +603,7 @@ class Swedbank_Pay_Api {
 				break;
 			case 'processing':
 			case 'completed':
-				if ( ! $order->is_paid() ) {
+				if ( empty( $order->get_date_paid() ) ) {
 					$order->payment_complete( $transaction_id );
 					if ( $message ) {
 						$order->add_order_note( $message );
@@ -629,7 +624,7 @@ class Swedbank_Pay_Api {
 
 				break;
 			case 'cancelled':
-				// Set cancelled
+				// Set cancelled.
 				if ( ! $order->has_status( 'cancelled' ) ) {
 					$order->update_status( 'cancelled', $message );
 				} elseif ( $message ) {
@@ -657,6 +652,8 @@ class Swedbank_Pay_Api {
 			default:
 				$order->update_status( $status, $message );
 		}
+
+		$order->save();
 	}
 
 	/**
