f3767a01e9cd7783c85805574286e57e981cef62
Montazar <github.v0its@slmail.me>
2025-10-23 14:28:34 +0200

    Update class-swedbank-pay-plugin.php

--- includes/class-swedbank-pay-plugin.php
+++ includes/class-swedbank-pay-plugin.php
@@ -189,7 +189,7 @@ class Swedbank_Pay_Plugin {
 				'Settings',
 				'swedbank-pay-woocommerce-checkout'
 			) . '</a>',
-			'<a href="' . esc_url( 'https://krokedil.com/support?plugin=316450&utm_source=swedbank-pay&utm_medium=docs&utm_campaign=mainpage' ) . '">' . __(
+			'<a href="' . esc_url( 'https://krokedil.com/support/?plugin=316450&utm_source=swedbank-pay&utm_medium=wp-admin' ) . '">' . __(
 				'Support',
 				'swedbank-pay-woocommerce-checkout'
 			) . '</a>',
